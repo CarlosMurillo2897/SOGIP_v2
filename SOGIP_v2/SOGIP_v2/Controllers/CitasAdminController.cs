b8811eae955ec3cb9ed0de013436c8cd5c7f4388
Lisandra <lisandra.agueroruiz@gmail.com>
2018-10-11 22:00:06 -0600

    Cita
    Cita ->solo vista desde la BD
    Horario->Si inserta

---
+++ SOGIP_v2/SOGIP_v2/Controllers/CitasAdminController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SOGIP_v2.Models;
+
+namespace SOGIP_v2.Controllers
+{
+    public class CitasAdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: CitasAdmin
+        public ActionResult Index()
+        {
+            return View(db.Cita.ToList());
+        }
+
+        public JsonResult GetEvents()
+        {
+            var Citas = db.Cita.ToList();
+            return new JsonResult { Data = Citas, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            
+        }
+
+        // GET: CitasAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cita cita = db.Cita.Find(id);
+            if (cita == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cita);
+        }
+
+        // GET: CitasAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CitasAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CitaId,InBody,Otro")] Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cita.Add(cita);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cita);
+        }
+
+        // GET: CitasAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cita cita = db.Cita.Find(id);
+            if (cita == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cita);
+        }
+
+        // POST: CitasAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CitaId,InBody,Otro")] Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cita).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cita);
+        }
+
+        // GET: CitasAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cita cita = db.Cita.Find(id);
+            if (cita == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cita);
+        }
+
+        // POST: CitasAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cita cita = db.Cita.Find(id);
+            db.Cita.Remove(cita);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
