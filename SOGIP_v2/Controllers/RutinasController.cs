9af68be6b39ebee66e1f0c39b39db80a0e797896
Adriana <caballerosbadri@gmail.com>
2018-10-20 17:57:18 -0600

    rut3

--- SOGIP_v2/Controllers/RutinasController.cs
+++ SOGIP_v2/Controllers/RutinasController.cs
@@ -80,7 +80,7 @@ namespace SOGIP_v2.Controllers
                 };
                 db.Conjunto_Ejercicios.Add(conjunto);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+        
 
             }
 
