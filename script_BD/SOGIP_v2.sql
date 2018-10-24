b901bf2b34699fa1042514e53c2ea794448e19ec
Carlos Murillo <cmb2897@gmail.com>
2018-10-24 14:02:54 -0600

    Creación de select, delete (limpiar tablas), usuarios predeterminados y roles.

--- script_BD/SOGIP_v2.sql
+++ script_BD/SOGIP_v2.sql
@@ -10,7 +10,7 @@ use "SOGIP_v3"
 
 +++++++ RECONSTRUIR BASE DE DATOS CON LOS CAMBIOS RESPECTIVOS ++++++++++
 
--- ++++++++++++++++++++++++++++ Select's ++++++++++++++++++++++++++++
+ ++++++++++++++++++++++++++++ Select's ++++++++++++++++++++++++++++
 
 
  select SOGIP_Users.Id, SOGIP_Users.Nombre1, SOGIP_Users.Fecha_Nacimiento, SOGIP_Users.Cedula, SOGIP_Roles.Name, SOGIP_Roles.id
@@ -25,37 +25,69 @@ use "SOGIP_v3"
  delete from SOGIP_Users where Email != 'cmb28@hotmail.com';
  delete from SOGIP_Users;
 
- select * from SOGIP_Users;
- drop table SOGIP_Estados;
- select * from SOGIP_Roles order by Id asc;
-
- select * from SOGIP_UserRoles, SOGIP_Roles;
- select * from SOGIP_Estados;
-
-insert into SOGIP_Users values('aab4f4cd-060b-4755-8439-dcb1321442db','123456789',NULL,'2018-10-14 17:30:50.793','Carlos','Isaias','Murillo','Badilla','1997-08-28 00:00:00.000',1,'joha@yahos.com',0,'AIyOJ27uVPdqtXxCKXPVcZYYRFs5tW5BRq/F8ZaZnBFanUZRvJjAB7iCD70wVB1b1w==','3285f946-2df7-4ffa-ab40-b0fc82464f32',NULL,0,0,NULL,1,0,'123456789');
-
-drop table SOGIP_Cita;
-
- select * from SOGIP_Selecciones;
- select * from SOGIP_Entrenadores;
- select * from SOGIP_Atletas;
- select * from SOGIP_Funcionario_ICODER;	
- select * from SOGIP_Entidad_Publica;
- select * from SOGIP_Asociacion_Deportiva;
- select * from SOGIP_Expedientes_Fisicos;
-
  select * from SOGIP_Archivo;
+ select * from SOGIP_Asociacion_Deportiva;
+ select * from SOGIP_Atletas;
+ select * from SOGIP_Categorias;
+ select * from SOGIP_Cita;
+ select * from SOGIP_Conjunto_Ejercicio;
+ select * from SOGIP_Deportes;
+ select * from SOGIP_Entidad_Publica;
+ select * from SOGIP_Entrenadores;
+ select * from SOGIP_Estados;
+ select * from SOGIP_Expedientes_Fisicos;
+ select * from SOGIP_Funcionario_ICODER;
+ select * from SOGIP_Horario;
+ select * from SOGIP_Roles order by Id asc;
+ select * from SOGIP_Rutina;
+ select * from SOGIP_Selecciones;
+ select * from SOGIP_Tipo_Deporte;
+ select * from SOGIP_Tipo_Entidad;
+ select * from SOGIP_UserClaims;
+ select * from SOGIP_UserLogins;
+ select * from SOGIP_UserRoles;
+ select * from SOGIP_Users;
 
- sp_help SOGIP_Users; -- Describe los atributos de cualquier tabla.
- sp_help SOGIP_Entrenadores;
+ -- Describe los atributos de cualquier tabla. --
 
-*/
+		sp_help SOGIP_Users; 
+		sp_help SOGIP_Entrenadores;
 
--- ++++++++++++++++++++++++++++ Select's ++++++++++++++++++++++++++++
+insert into SOGIP_Users values('b889fe47-541d-4453-8733-022709c1592f', '114070986', NULL, '2019-01-18 15:52:12.557', 'Josafat', 'Antonio', 'Barboza', 'Umaña', '1989-10-06 00:00:00.000', 0, 1, 'josa@josa.com', 0, 'AAAAGb+hoJnZqkFtRi3SDPAQqwviIyybQISr4nF99nLf9cEoygh0rzKIUm2Dx1qYyQ==', 'de3fb782-d243-4a25-8106-623ebfa68fc7', NULL, 0, 0, NULL, 1, 0, '114070986');
 
--- ++++++++++++++++++++++++++ Trigger's ++++++++++++++++++++++++++
 
-/*
+ ++++++++++++++++++++++++++++ Select's ++++++++++++++++++++++++++++
+
+ ++++++++++++++++++++++++++++ Delete's ++++++++++++++++++++++++++++
+
+ delete from SOGIP_Archivo;
+ delete from SOGIP_Asociacion_Deportiva;
+ delete from SOGIP_Atletas;
+ delete from SOGIP_Categorias;
+ delete from SOGIP_Cita;
+ delete from SOGIP_Conjunto_Ejercicio;
+ delete from SOGIP_Deportes;
+ delete from SOGIP_Entidad_Publica;
+ delete from SOGIP_Entrenadores;
+ delete from SOGIP_Estados;
+ delete from SOGIP_Expedientes_Fisicos;
+ delete from SOGIP_Funcionario_ICODER;
+ delete from SOGIP_Horario;
+ delete from SOGIP_Roles order by Id asc;
+ delete from SOGIP_Rutina;
+ delete from SOGIP_Selecciones;
+ delete from SOGIP_Tipo_Deporte;
+ delete from SOGIP_Tipo_Entidad;
+ delete from SOGIP_UserClaims;
+ delete from SOGIP_UserLogins;
+ delete from SOGIP_UserRoles;
+ delete from SOGIP_Users;
+
+ ++++++++++++++++++++++++++++ Delete's ++++++++++++++++++++++++++++
+
+ ++++++++++++++++++++++++++ Trigger's ++++++++++++++++++++++++++
+
+
 create trigger fecha_expiracion on SOGIP_Users
  for update, insert
   as
@@ -66,11 +98,12 @@ create trigger fecha_expiracion on SOGIP_Users
      from inserted
      where SOGIP_Users.id = inserted.id
     end
+
+ ++++++++++++++++++++++++++ Trigger's ++++++++++++++++++++++++++
+
+ ++++++++++++++++++++++++++++ Insert's ++++++++++++++++++++++++++++
+
 */
-
--- ++++++++++++++++++++++++++ Trigger's ++++++++++++++++++++++++++
-
--- ++++++++++++++++++++++++++++ Insert's ++++++++++++++++++++++++++++
 
 insert into SOGIP_Roles values('1', 'Supervisor');
 insert into SOGIP_Roles values('2', 'Administrador');
@@ -198,19 +231,17 @@ insert into SOGIP_Tipo_Entidad values('Universidad Empresarial de Costa Rica');
 insert into SOGIP_Tipo_Entidad values('Universidad Estatal a Distancia');
 insert into SOGIP_Tipo_Entidad values('Universidad Nacional de Costa Rica');
 
-/*
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-11-18 08:00:11',1),convert(datetime,'10-11-18 08:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-11-18 09:00:11',1),convert(datetime,'10-11-18 09:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-09-18 10:00:11',1),convert(datetime,'10-09-18 10:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-09-18 11:00:11',1),convert(datetime,'10-09-18 11:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-23-18 08:00:11',1),convert(datetime,'10-23-18 08:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-23-18 09:00:11',1),convert(datetime,'10-23-18 09:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'11-01-18 08:00:11',1),convert(datetime,'11-01-18 08:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'11-01-18 09:00:11',1),convert(datetime,'11-01-18 09:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-12-18 08:00:11',1),convert(datetime,'10-12-18 08:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-12-18 09:00:11',1),convert(datetime,'10-12-18 09:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-12-18 10:00:11',1),convert(datetime,'10-12-18 11:25:11',1));
-insert into sogip_cita values(1,1,'68f77a05-90b7-43a6-ad6e-604837744662', convert(datetime,'10-12-18 13:00:11',1),convert(datetime,'10-12-18 13:25:11',1));
-*/
+insert into SOGIP_Users values('43002ea0-b75c-44a0-8f29-7d5c0eeffed2', '112530907', NULL, '2019-01-18 16:32:10.283', 'Emmanuel', 'David', 'Chanto', 'Segura', '1985-08-25 00:00:00.000', 0, 1, 'chanto@chanto.com', 0, 'AOP0cWhiVc+Eot6ruccEtMvhqwrSt6K6bXixvSTtOryoGNt8TP8k7/ETtBrPjlgACA==', 'f24f4272-ceeb-4f08-bf54-611817c49847', NULL, 0, 0, NULL, 1, 0, '112530907');
+insert into SOGIP_Users values('914db4cb-8e02-4476-9e42-31befefd7a0e', '402120310', NULL, '2019-01-18 17:08:56.407', 'Ana', 'Maria', 'Porras', 'Loria', '1991-09-21 00:00:00.000', 0, 1, 'anaporras@anaporras.com', 0, 'AMJoytp/8v+RwnlaWrtK4MDwxUGWwBdJ+/XNbpQbtRo0bC2eTf3uMs5rX0iHsus1ng==', 'e4a8b6c5-eb52-4c07-aeae-48216b8b286e', NULL, 0, 0, NULL, 1, 0, '402120310');
+insert into SOGIP_Users values('9d9d279f-016a-47e9-bf70-9ed4a4754de5', '110830174', NULL, '2019-01-18 17:07:06.053', 'Aaron', 'Andres', 'Hidalgo', 'Nuñez', '1980-02-10 00:00:00.000', 0, 1, 'aaron@aaron.com', 0, 'AI33t6s1zh79xQNvhIZu+a3ew6AP+qhorBRdp0Q8xFn6bhhU4cemPKEE8t334VC2aA==', 'b8589e3c-e75c-47a0-9dfc-5e56ea5582eb', NULL, 0, 0, NULL, 1, 0, '110830174');
+insert into SOGIP_Users values('b889fe47-541d-4453-8733-022709c1592f', '114070986', NULL, '2019-01-18 15:52:12.557', 'Josafat', 'Antonio', 'Barboza', 'Umaña', '1989-10-06 00:00:00.000', 0, 1, 'josa@josa.com', 0, 'AAAAGb+hoJnZqkFtRi3SDPAQqwviIyybQISr4nF99nLf9cEoygh0rzKIUm2Dx1qYyQ==', 'de3fb782-d243-4a25-8106-623ebfa68fc7', NULL, 0, 0, NULL, 1, 0, '114070986');
+insert into SOGIP_Users values('de8b4ac7-40a3-4b23-aa2b-28ae9fcb9253', '402260033', NULL, '2019-01-18 17:13:38.123', 'Emmanuel', NULL, 'Niño', 'Villalta', '1995-02-14 00:00:00.000', 0, 1, 'emmanuelnino@emmanuelnino.com', 0, 'AArLIlxJ6VcgWrGQRqlVf8jM1UA7+WoOndCCiMZ3D6bJJQ2KI8MiBLmttJCQSL/3Hg==', '93511ecb-cb2b-4a6c-8191-81dcfdc47ced', NULL, 0, 0, NULL, 1, 0, '402260033');
+insert into SOGIP_Users values('8f9c47bf-edbd-40bf-9b5e-f753dd81a766', '000000000', NULL, '2019-01-19 21:34:58.363', 'Sistema', 'Operativo', 'Gimnasio', 'Pesas', '2000-01-01 00:00:00.000', 0, 1, 'admSOGIP@hotmail.com', 0, 'AG33oMVrb494bn6JdvpIGf2UV1wbopV1ttoNqJP9/LOD8S0PPltbD4XUYzMorUX8mA==', '6508b809-9b26-4f7e-aad6-b550cbe32fa6', NULL, 0, 0, NULL, 1, 0, '000000000');
+
+insert into SOGIP_UserRoles values('8f9c47bf-edbd-40bf-9b5e-f753dd81a766', 1);
+insert into SOGIP_UserRoles values('b889fe47-541d-4453-8733-022709c1592f', 2);
+insert into SOGIP_UserRoles values('43002ea0-b75c-44a0-8f29-7d5c0eeffed2', 3);
+insert into SOGIP_UserRoles values('de8b4ac7-40a3-4b23-aa2b-28ae9fcb9253', 6);
+
 
 -- ++++++++++++++++++++++++++ Insert's ++++++++++++++++++++++++++
