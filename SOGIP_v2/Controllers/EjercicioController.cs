fd3d45afe83190ff1407c4d8e661dd2d9da42df7
Adriana <caballerosbadri@gmail.com>
2019-03-02 23:03:45 -0600

    nuevos

--- SOGIP_v2/Controllers/EjercicioController.cs
+++ SOGIP_v2/Controllers/EjercicioController.cs
@@ -17,14 +17,62 @@ namespace SOGIP_v2.Controllers
         {
             return View();
         }
-        public JsonResult SaveEjercicio(string nombre)
+        public ActionResult TipoEjercicio()
         {
-            Ejercicio ejercicio = new Ejercicio();
+            return View();
+        }
+        public JsonResult ObtenerTipo(int id)
+        {
+
+            var ejercicio = db.Ejercicio.Where(a => a.Id == id).FirstOrDefault();
+
+            return Json(ejercicio, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult DeleteEjercicioTipo(int id)
+        {
+            var status = false;
+            var v = db.Ejercicio.Where(a => a.Id == id).FirstOrDefault();
+            if (v != null)
+            {
+                var tipos = db.Ejercicio.Where(x => x.EjercicioId == id).ToList();
+                foreach (var n in tipos)
+                {
+                    int i = n.Id;
+                    Ejercicio tipo = db.Ejercicio.Find(i);
+                    db.Ejercicio.Remove(tipo);
+                }
+                db.Ejercicio.Remove(v);
+                db.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+
+        }
+        public JsonResult EditTipo(int id, string nombre)
+        {
+            Ejercicio ejercicio = db.Ejercicio.Single(x => x.Id == id);
+            try
+            {
+                if (ejercicio != null)
+                {
+                    ejercicio.Nombre = nombre;
+                }
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            return Json(ejercicio, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult SaveEjercicioTipo(string nombre)
+        {
+            Ejercicio ejercicio= new Ejercicio();
             try
             {
                 if (nombre != null)
                 {
-                    ejercicio.Descripcion = nombre;
+                    ejercicio.Nombre= nombre;
                     db.Ejercicio.Add(ejercicio);
                 }
                 db.SaveChanges();
@@ -37,8 +85,86 @@ namespace SOGIP_v2.Controllers
         }
         public JsonResult GetTipos()
         {
-            var Ejercicios = db.Ejercicio.ToList();
-            return Json(Ejercicios, JsonRequestBehavior.AllowGet);
+            var data = from a in db.Ejercicio
+                       where a.EjercicioId == 0
+                       select new
+                       {
+                           Descripcion = a.Nombre,
+                           Id = a.Id
+                       };
+            var Tipo = data.ToList();
+            return Json(Tipo, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetTipo()
+        {
+            var data = from a in db.Ejercicio
+                       where a.EjercicioId == 0
+                       select new
+                       {
+                           Accion = "",
+                           Descripcion = a.Nombre,
+                           Id = a.Id
+                       };
+            var maquinas = data.ToList();
+            return Json(maquinas, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult ObtenerEjercicio(int id)
+        {
+            var ejercicio = db.Ejercicio.Where(a => a.Id == id).FirstOrDefault();
+            var nom = ejercicio.Nombre;
+            var tipo = db.Ejercicio.Where(x => x.Id == ejercicio.EjercicioId).Select(y => y.Nombre).FirstOrDefault();
+            var data = new { Nombre = nom, Tipo = tipo };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult DeleteEjercicio(int id)
+        {
+            var status = false;
+            var v = db.Ejercicio.Where(a => a.Id == id).FirstOrDefault();
+            if (v != null)
+            {
+                db.Ejercicio.Remove(v);
+                db.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+
+        }
+        public JsonResult SaveEjercicio(string Nombre, int Tipo)
+        {
+            Ejercicio ejercicio = db.Ejercicio.Single(x => x.Id == Tipo);
+            Ejercicio ejercicio2 = new Ejercicio();
+            try
+            {
+                if (ejercicio != null)
+                {
+                    ejercicio2.EjercicioId = ejercicio.Id;
+                    ejercicio2.Nombre = Nombre;
+                    db.Ejercicio.Add(ejercicio2);
+                }
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            return Json(ejercicio2, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetEjercicios()
+        {
+
+            var data = from a in db.Ejercicio
+                       where a.EjercicioId != 0
+                       select new
+                       {
+                           Tipo = db.Ejercicio.Where(x => x.Id == a.EjercicioId).Select(y => y.Nombre).FirstOrDefault(),
+                           Nombre = a.Nombre,
+                           Id = a.Id
+
+                       };
+            var Maquina = data.ToList();
+            return Json(Maquina, JsonRequestBehavior.AllowGet);
 
         }
     }
--- SOGIP_v2/Controllers/MaquinaController.cs
+++ SOGIP_v2/Controllers/MaquinaController.cs
@@ -17,8 +17,31 @@ namespace SOGIP_v2.Controllers
         {
             return View();
         }
-        
-      public JsonResult SaveMaquina(string nombre)
+        public ActionResult TipoMaquina()
+        {
+            return View();
+        }
+        public JsonResult SaveMaquina(string Nombre,int Tipo)
+        {
+            Maquina maquina = db.Maquina.Single(x => x.Id == Tipo);
+            Maquina maquina2 = new Maquina();
+            try
+            {
+                if (maquina != null)
+                {
+                    maquina2.MaquinaId = maquina.Id;
+                    maquina2.Descripcion = Nombre;
+                    db.Maquina.Add(maquina2);
+                }
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            return Json(maquina2, JsonRequestBehavior.AllowGet);
+        }
+      public JsonResult SaveMaquinaTipo(string nombre)
         {
             Maquina maquina = new Maquina();
             try
@@ -36,10 +59,113 @@ namespace SOGIP_v2.Controllers
             }
             return Json(maquina, JsonRequestBehavior.AllowGet);
         }
+        public JsonResult DeleteMaquinaTipo(int id)
+        {
+            var status = false;
+            var v = db.Maquina.Where(a => a.Id == id).FirstOrDefault();
+            if (v != null)
+            {
+                var tipos = db.Maquina.Where(x => x.MaquinaId == id).ToList();
+                foreach(var n in tipos)
+                {
+                    int i = n.Id;
+                    Maquina tipo = db.Maquina.Find(i);
+                    db.Maquina.Remove(tipo);
+                }
+                db.Maquina.Remove(v);
+                db.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+
+        }
+        public JsonResult DeleteMaquina(int id)
+        {
+            var status = false;
+            var v = db.Maquina.Where(a => a.Id == id).FirstOrDefault();
+            if (v != null)
+            {
+                db.Maquina.Remove(v);
+                db.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+
+        }
+        public JsonResult GetMaquinas()
+        {
+
+            var data = from a in db.Maquina
+                       where a.MaquinaId !=0
+                       select new
+                       {
+                           Tipo = db.Maquina.Where(x => x.Id==a.MaquinaId).Select(y => y.Descripcion).FirstOrDefault(),
+                           Nombre = a.Descripcion,
+                           Id = a.Id
+
+                       };
+            var Maquina = data.ToList();           
+            return Json(Maquina, JsonRequestBehavior.AllowGet);
+
+        }
         public JsonResult GetTipos()
         {
-            var Maquina = db.Maquina.ToList();           
-            return Json(Maquina, JsonRequestBehavior.AllowGet);
+            var data = from a in db.Maquina
+                       where a.MaquinaId == 0
+                       select new
+                       {
+                           Descripcion = a.Descripcion,
+                           Id = a.Id
+                       };
+            var Tipo = data.ToList();
+            return Json(Tipo, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ObtenerTipo(int id)
+        {
+           
+            var maquina = db.Maquina.Where(a => a.Id == id).FirstOrDefault();
+           
+            return Json(maquina, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ObtenerMaquina(int id)
+        {
+            var maquina = db.Maquina.Where(a => a.Id == id).FirstOrDefault();
+            var nom = maquina.Descripcion;
+            var tipo = db.Maquina.Where(x => x.Id == maquina.MaquinaId).Select(y => y.Descripcion).FirstOrDefault();
+            var data =  new {Nombre = nom, Tipo = tipo };
+           
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult EditTipo(int id, string nombre)
+        {
+            Maquina maquina = db.Maquina.Single(x => x.Id == id);
+            try
+            {
+                if (maquina != null)
+                {
+                    maquina.Descripcion= nombre;
+                }
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            return Json(maquina, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetTipo()
+        {
+            var data = from a in db.Maquina
+                       where a.MaquinaId == 0
+                       select new
+                       {
+                           Accion ="",
+                           Descripcion = a.Descripcion,
+                            Id = a.Id
+                       };
+            var maquinas = data.ToList();
+            return Json(maquinas, JsonRequestBehavior.AllowGet);
 
         }
     }
