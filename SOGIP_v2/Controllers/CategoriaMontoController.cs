449ca917b31e9be57c4ec8459a2965e1c282232b
Adriana <caballerosbadri@gmail.com>
2019-03-24 11:49:13 -0600

    commit

--- SOGIP_v2/Controllers/CategoriaMontoController.cs
+++ SOGIP_v2/Controllers/CategoriaMontoController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SOGIP_v2.Models;
 namespace SOGIP_v2.Controllers
 {
     public class CategoriaMontoController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: CategoriaMonto
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult GetCategorias()
+        {
+            var lista = db.Roles.ToList();
+            return Json(lista, JsonRequestBehavior.AllowGet);
         }
     }
 }
