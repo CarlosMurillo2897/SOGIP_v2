fe064df75805898798c1279bc86f63dcd44ea51f
Adriana <caballerosbadri@gmail.com>
2019-04-17 21:14:31 -0600

    commit

--- SOGIP_v2/Controllers/TipoMEController.cs
+++ SOGIP_v2/Controllers/TipoMEController.cs
@@ -58,17 +58,38 @@ namespace SOGIP_v2.Controllers
         }
         public JsonResult getCategorias()
         {
-            var consulta = db.TipoME.Where(x=>x.TipoId == 0).ToList();
+            var consulta = from t in db.TipoME.Where(x => x.TipoId == 0)
+                           select new
+                           {
+                               t.nombre,
+                               t.Id
+                           };
+            return Json(consulta.ToList(), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult getNombreCat(string nombre)
+        {
+            var consulta = db.TipoME.Where(x => x.nombre == nombre).FirstOrDefault();
             return Json(consulta, JsonRequestBehavior.AllowGet);
         }
         public JsonResult ObtenerEjer(int n)
         {
-            var consulta = db.TipoME.Where(x => x.TipoId == n).ToList();
-            return Json(consulta, JsonRequestBehavior.AllowGet);
+
+            var consulta = from t in db.TipoME.Where(x => x.TipoId == n)
+                           select new
+                           {
+                               t.nombre,
+                               t.Id
+                           };
+            return Json(consulta.ToList(), JsonRequestBehavior.AllowGet);
         }
         public JsonResult TipoRepetido(string nombre)
         {
             return Json(!db.TipoME.Any(x => x.nombre == nombre), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult getNombre(int id)
+        {
+            var consulta = db.TipoME.Where(x => x.Id == id).FirstOrDefault();
+            return Json(consulta, JsonRequestBehavior.AllowGet);
         }
 
     }
